31174756223261d72bc7e8cf9f48c0718f1cd899
jm.yakan@gmail.com <jm.yakan@gmail.com>
2025-01-24 15:54:29 +0100

    Deploy me to Vercel front

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-console.log("Script OK")
+console.log("test ok")
 
-fetch('http://localhost:3000/years', { })
+fetch('http://localhost:3000/year')
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-    //   (document.querySelector("#year").textContent = ${req.data.year})
+    //   console.log(data);
+      document.querySelector("#year").textContent = data.year;
+    })
+;
+
+fetch('https://whatyearisit-backend-plum-zeta.vercel.app/year')
+    .then(response => response.json())
+    .then(data => {
+    //   console.log(data);
+      document.querySelector("#year").textContent = data.year;
     })
 ;
